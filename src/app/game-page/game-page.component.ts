7f99990a080e1ae9471e8a043d4e8d310ef7c7b1
jcsorgei <csjozef88@gmail.com>
2022-05-21 18:45:09 +0200

    feat(game-page): implemented card flipping logic

--- src/app/game-page/game-page.component.ts
+++ src/app/game-page/game-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 import { GameService } from '../_services/game.service';
 
 @Component({
@@ -10,9 +10,13 @@ export class GamePageComponent implements OnInit {
   numberOfCards: number = 10;
 
   deckSize: number | any = 4;
-  cardsOnBoard: number[] = [];
+  board: {cardNumbers: number[]; cardStates: boolean[]} = {cardNumbers: [], cardStates: []};
   currentTries: number = 0;
   best: number = 0;
+
+  firstCardIndex: number = 0;
+  secondCardIndex: number = 0;
+  firstFlip: boolean = true;
 
   constructor(private gameService: GameService) {}
 
@@ -26,15 +30,45 @@ export class GamePageComponent implements OnInit {
 
   initializeBoard(){
     for (let i=1;i<=this.deckSize;i++) {
-      this.cardsOnBoard.push(i,i);
+      this.board.cardNumbers.push(i,i);
+    }
+    for (let i=1;i<=this.deckSize;i++) {
+      this.board.cardStates.push(false,false);
     }
 
-    this.shuffleArray(this.cardsOnBoard);
-    console.log(this.cardsOnBoard);
+    this.shuffleArray(this.board.cardNumbers);
+    console.log(this.board.cardNumbers);
   }
 
   shuffleArray(arr: number[]){
     arr.sort(() => (Math.random() > .5) ? 1 : -1);
   }
 
+  flipCard(cardIndex: number){
+    console.log(this.firstFlip);
+    console.log(this.board.cardNumbers[cardIndex])
+    if (this.firstFlip){
+      this.firstFlip = false;
+      this.firstCardIndex = cardIndex;
+      this.board.cardStates[cardIndex] = true;
+    } else{
+      console.log("lefut")
+      this.firstFlip = true;
+      this.secondCardIndex = cardIndex;
+      this.board.cardStates[cardIndex] = true;
+
+      if (this.board.cardNumbers[this.firstCardIndex] !== this.board.cardNumbers[this.secondCardIndex]){
+        console.log("not equal")
+        setTimeout(() => {
+          this.board.cardStates[this.firstCardIndex] = false;
+          this.board.cardStates[this.secondCardIndex] = false;
+        }, 1200);
+      }
+    }
+
+
+
+    
+
+  }
 }
